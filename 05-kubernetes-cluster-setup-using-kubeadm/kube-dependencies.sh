6893b400f16ca5d3cd8a7b8f03caa9910cb8e643
kevinrizki1019 <13518100@std.stei.itb.ac.id>
2023-03-29 15:17:08 +0700

    Update kube-dependencies

--- 05-kubernetes-cluster-setup-using-kubeadm/kube-dependencies.sh
+++ 05-kubernetes-cluster-setup-using-kubeadm/kube-dependencies.sh
@@ -11,89 +11,96 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 # Add Kubernetes repository for Ubuntu 20.04
-apt -y install curl apt-transport-https git wget
+sudo apt update
+sudo apt -y full-upgrade
+
+# Check kubelet, kubeadm, and kubectl before installing
+sudo apt -y install curl apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-# Update package list
-apt update -y
-
-# Check kubelet, kubeadm, and kubectl before installing
-apt-cache madison kubelet
-apt-cache madison kubeadm
-apt-cache madison kubectl
-
 # Install the Kubernetes with specify version
-apt install -y kubelet=${KUBERNETES_VERSION} kubeadm=${KUBERNETES_VERSION} kubectl=${KUBERNETES_VERSION}
+sudo apt update
+sudo apt -y install vim git curl wget kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 
 # Confirm installation by checking the version of kubectl
 kubectl version --client && kubeadm version
 
 # Disable swap
-swapon --show
-swapoff -a
-rm /swap.img
-sed -i '/\/swap\.img/d' /etc/fstab
-swapon --show
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo sed -i '/swap/s/^/#/' /etc/fstab
 
+# Confirm setting is correct
+sudo swapoff -a
+sudo mount -a
+free -h
 
-# Enable the kernel modules
-modprobe overlay
-modprobe br_netfilter
+# Enable kernel modules
+sudo modprobe overlay
+sudo modprobe br_netfilter
 
-# Configure sysctl 
-echo "net.bridge.bridge-nf-call-ip6tables = 1" >> /etc/sysctl.d/kubernetes.conf
-echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.d/kubernetes.conf
-echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/kubernetes.conf
+# Add some settings to sysctl
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
 
-sysctl --system
+# Reload sysctl
+sudo sysctl --system
 
-# Install Docker Container Runtime
-apt update
-apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Add repo and Install packages
+sudo apt update
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y containerd.io docker-ce docker-ce-cli
 
-# Create the required directories
-mkdir -p /etc/systemd/system/docker.service.d
-mkdir /etc/docker
-echo "{" > /etc/docker/daemon.json
-echo "  "exec-opts": ["native.cgroupdriver=systemd"]," >> /etc/docker/daemon.json
-echo "  "log-driver": "json-file"," >> /etc/docker/daemon.json
-echo "  "log-opts": {" >> /etc/docker/daemon.json
-echo "  "exec-opts": ["native.cgroupdriver=systemd"]," >> /etc/docker/daemon.json
-echo "      "max-size": "100m"" >> /etc/docker/daemon.json
-echo "  }," >> /etc/docker/daemon.json
-echo "  "storage-driver": "overlay2"" >> /etc/docker/daemon.json
-echo "}" >> /etc/docker/daemon.json
+# Create required directories
+sudo mkdir -p /etc/systemd/system/docker.service.d
 
-# Enable the Docker service
-systemctl daemon-reload
-systemctl restart docker
-systemctl enable docker
+# Create daemon json config file
+sudo tee /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
 
-# Make sure the Docker service is running
-systemctl status docker
+# Start and enable Services
+sudo systemctl daemon-reload 
+sudo systemctl restart docker
+sudo systemctl enable docker
 
-# Install Mirantis cri-dockerd
+# https://computingforgeeks.com/install-mirantis-cri-dockerd-as-docker-engine-shim-for-kubernetes/
+### Debian based systems ###
+sudo apt update
+sudo apt install git wget curl
+
 VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest|grep tag_name | cut -d '"' -f 4|sed 's/v//g')
 echo $VER
+
+### For Intel 64-bit CPU ###
 wget https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz
 tar xvf cri-dockerd-${VER}.amd64.tgz
-mv cri-dockerd/cri-dockerd /usr/local/bin/
+
+sudo mv cri-dockerd/cri-dockerd /usr/local/bin/
+
 cri-dockerd --version
+
 wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service
 wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket
-mv cri-docker.socket cri-docker.service /etc/systemd/system/
-sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+sudo mv cri-docker.socket cri-docker.service /etc/systemd/system/
+sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
 
-# Enable the cri-dockerd service
-systemctl daemon-reload
-systemctl enable cri-docker.service
-systemctl enable --now cri-docker.socket
+sudo systemctl daemon-reload
+sudo systemctl enable cri-docker.service
+sudo systemctl enable --now cri-docker.socket
 
-# Make sure the cri-dockerd is running
-systemctl status cri-docker
+systemctl status cri-docker.socket
